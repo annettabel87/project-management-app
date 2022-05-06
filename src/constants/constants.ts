ffe9a0028c5a05226a41a48841dd998601d7f527
annettabel87 <annettabel@rambler.ru>
2022-05-06 22:57:07 +0500

    feat: add api endpoints

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,3 +6,11 @@ export const routers = {
   ROUTE_PROFILE: '/profile',
   ROUTE_NOTFOUND: '*',
 };
+
+export const api_endpoints = {
+  USERS: 'https://kanban71.herokuapp.com/users',
+  BOARDS: 'https://kanban71.herokuapp.com/boards',
+  SIGNIN: 'https://kanban71.herokuapp.com/signin',
+  SIGNUP: 'https://kanban71.herokuapp.com/signup',
+  FILE: 'https://kanban71.herokuapp.com/file',
+};
