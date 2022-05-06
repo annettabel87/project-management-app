de76fc8b394c75e7749b77f91020f1d4b1e94f31
annettabel87 <annettabel@rambler.ru>
2022-05-06 17:34:51 +0500

    feat: sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { routers } from '../../constants/constants';
+import { useState } from 'react';
 import './Header.css';
 
 export const Header: FC = () => {
+  const [sticky, setSticky] = useState<boolean>(false);
+  const setSticked = () => {
+    if (window.scrollY >= 100) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('scroll', setSticked);
+
+    return () => {
+      document.removeEventListener('scroll', setSticked);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={sticky ? 'header sticky' : 'header'}>
       <NavLink className="header-item" to={routers.ROUTE_WELCOME}>
         Welcome
       </NavLink>
