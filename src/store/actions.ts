e7b5bcb488d31c2c20f98cbd3d36f6e66dc98282
annettabel87 <annettabel@rambler.ru>
2022-05-08 11:30:32 +0500

    fix: fix  post request to create board

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -4,12 +4,18 @@ import { api_endpoints } from '../constants/constants';
 import { boardsSlice } from './boardReducer';
 
 export const addBoard = createAsyncThunk('board/fetch', async (title: string, { dispatch }) => {
-  //   dispatch(boardsSlice.actions.dataFetching());
-  //   axios({
-  //     method: 'post',
-  //     url: api_endpoints.BOARDS,
-  //     data: {
-  //       title,
-  //     },
-  //   }).catch(() => dispatch(boardsSlice.actions.dataFetchingError('Failed to create')));
+  dispatch(boardsSlice.actions.dataFetching());
+  axios
+    .post(
+      api_endpoints.BOARDS,
+      { title },
+      {
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer token`,
+        },
+      }
+    )
+    .then((data) => console.log(data.data))
+    .catch(() => dispatch(boardsSlice.actions.dataFetchingError('Failed to create')));
 });
