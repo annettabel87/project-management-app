74db1c3d2744ac807e3ce053214e75e1a98d2d03
annettabel87 <annettabel@rambler.ru>
2022-05-13 23:51:05 +0500

    feat: add create board button

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,25 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { routers } from '../../constants/constants';
 import { useAppDispatch, useAppSelector } from '../../hooks/ReduxHooks';
 import { getBoards } from '../../store/actions';
+import { CreateBoardForm } from '../Header/CreateBoardForm/CreateBoardForm';
+import { Modal } from '../Modal/Modal';
 import { BoardsField } from './BoardsField/BoardsField';
 import './Main.css';
 
 export const Main: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const { boards, isLoading, error } = useAppSelector((state) => state.boardReducer);
   const isAuth = true;
   const dispatch = useAppDispatch();
-
+  const createBoard = () => {
+    setIsOpen(true);
+  };
+  const onClose = () => {
+    setIsOpen(false);
+  };
   useEffect(() => {
     dispatch(getBoards());
   }, [dispatch]);
@@ -21,6 +29,7 @@ export const Main: FC = () => {
   return (
     <div className="mainPage">
       <h2>Your boards</h2>
+      <button onClick={createBoard}>New board</button>
       {error ? (
         <span>{error}</span>
       ) : isLoading ? (
@@ -28,6 +37,9 @@ export const Main: FC = () => {
       ) : (
         <BoardsField boards={boards} />
       )}
+      <Modal onClose={onClose} open={isOpen}>
+        <CreateBoardForm isAuth={isAuth} onClose={onClose} />
+      </Modal>
     </div>
   );
 };
