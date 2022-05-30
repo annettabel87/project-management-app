bbf837c0db478f9fc96547aedc919f8c49802dd7
Annettabel87 <annettabel@rambler.ru>
2022-05-30 21:11:11 +0500

    feat: add welcome page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,8 +1,62 @@
 import { FC } from 'react';
-import './Welcome.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { ROUTERS } from '../../constants/constants';
+import { useAppSelector } from '../../hooks/ReduxHooks';
+import s from './Welcome.module.scss';
 
 const Welcome: FC = () => {
-  return <div>Welcome</div>;
+  const { token } = useAppSelector((state) => state.authorisationSlice);
+  const navigate = useNavigate();
+  return (
+    <div className={s.welcomePage}>
+      <section className={s.appInfo}>
+        <div className={s.container}>
+          <div className={s.welcomeHeader}>
+            {token ? (
+              <button className={s.welcomeBtn} onClick={() => navigate(ROUTERS.MAIN)}>
+                Go to Main Page
+              </button>
+            ) : (
+              <div className={s.btnWrapper}>
+                <button className={s.welcomeBtn} onClick={() => navigate(ROUTERS.LOGIN)}>
+                  Sign In
+                </button>
+                <button className={s.welcomeBtn} onClick={() => navigate(ROUTERS.REGISTRATION)}>
+                  Sign Up
+                </button>
+              </div>
+            )}
+          </div>
+          <div className={s.textWrapper}>
+            <h1 className={s.title}>Trello clone</h1>
+            <p className={s.description}>
+              Trello clone will help you organize your team&apos;s work. The user can set tasks,
+              complete tasks, view tasks, delete own tasks, be responsible for other people&apos;s
+              tasks
+            </p>
+          </div>
+          <div className={s.teamSection}>
+            <div className={s.card}>
+              <p>About command</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={s.appDescription}>
+        <div className={`${s.container} ${s.descriptionContainer}`}>
+          <div className={s.image}></div>
+          <div>
+            <ul>
+              <li className={s.item}>Add boards, columns and tasks</li>
+              <li className={s.item}>Manage your projects</li>
+              <li className={s.item}>Planning to work on a project</li>
+              <li className={s.item}>Develop team spirit</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Welcome;
